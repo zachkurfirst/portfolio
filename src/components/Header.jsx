94ffe38aa27e8442bf3deb1286974972a6f0da6b
Zach Kurfirst <zachmk11@gmail.com>
2023-12-11 22:18:29 -0500

    create basic about page - fetch data from about.json, basic aboutstyling, reposition nav, small updates to about data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ import "./Header.css";
 const Header = (props) => {
   return (
     <header>
-      <h1>My Portfolio</h1>
       <nav className="nav">
         <Link to="/">Home</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/about">About</Link>
       </nav>
+      <h1>My Portfolio</h1>
     </header>
   );
 };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,52 @@
+import { useState, useEffect } from "react";
+import "./About.css";
+
 const About = (props) => {
-  return <h1>About</h1>;
+  // create state to hold About data
+  const [about, setAbout] = useState(null);
+
+  // function to make API fetch call
+  const getAboutData = async () => {
+    try {
+      // make API call + get response
+      const response = await fetch("./about.json");
+      // convert response -> JSON
+      const data = await response.json();
+      // console.log("data:", data);
+      // set about state to API data
+      setAbout(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // make initial call for data so it only happens once on component load
+  useEffect(() => {
+    getAboutData();
+    // empty dependency array - run once - after initial render
+  }, []);
+
+  // const loaded = () => (
+  //   <div>
+  //     <img src={about.headshot} alt={about.name} />
+  //     <h2>{about.name}</h2>
+  //     <h3>{about.email}</h3>
+  //     <p>{about.bio}</p>
+  //   </div>
+  // );
+
+  // return about ? loaded() : <p>Loading...</p>;
+
+  return (
+    about && (
+      <div className="About">
+        <img src={about.headshot} alt={about.name} />
+        <h2>{about.name}</h2>
+        <h3>{about.email}</h3>
+        <p>{about.bio}</p>
+      </div>
+    )
+  );
 };
 
 export default About;
