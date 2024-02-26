576d251092e39660d4d6504fbf84873a03fb2710
Zach Kurfirst <zachmk11@gmail.com>
2024-02-26 10:44:00 -0500

    update button color design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 const Home = (props) => {
   return (
-    <section id="hero" className="bg-circuit-board dark:bg-circuit-board-dark w-full h-screen flex flex-col justify-center text-center">
+    <section id="hero" className="bg-circuit-board border-b-2 border-sky-950 dark:bg-circuit-board-dark w-full h-screen flex flex-col justify-center text-center">
       <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-sky-950 dark:text-sky-100 mb-4">
         Hello there!
       </h1>
